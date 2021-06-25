67e74ae93e4443fe06922a06a443b2c1461bb7c5
Akarsh Barar <barar.akarsh8@gmail.com>
2021-06-25 21:25:23 +0530

    Banner

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <Header/>
     <HeaderTwo/>
+    <div class="home">
+        <img class="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
+
+    </div>
   </div>
 </template>
 
@@ -17,5 +21,16 @@ export default {
 </script>
 
 <style>
-
+.home{
+  max-width:1500px ;
+  margin-left: auto;
+  margin-right: auto;
+  color: white;
+}
+.home__image{
+  mask-image: linear-gradient(to bottom,rgba(0,0,0,1),rgba(0,0,0,0));
+  width:100%;
+  margin-bottom: -150px;
+  z-index: -1;
+}
 </style>
