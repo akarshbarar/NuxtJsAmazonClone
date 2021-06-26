b51e22e1052f590261ccec26a03e5ad73e55a6c4
Akarsh Barar <barar.akarsh8@gmail.com>
2021-06-26 22:02:26 +0530

    Product added

---
+++ components/Product.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="product">
+        <div class="product__info">
+            <p class="product__title">Item 1</p>
+            <p class="product__price">Price 1</p>
+            <div class="product__rating">
+                <p>3.5</p>
+            </div>
+        </div>
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="image"/>
+        <button class="">Add to cart</button>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+.product__title{
+    text-align: center;
+    font-weight: 80;
+    font-size: xx-large;
+}
+.product__rating{
+    display: flex;
+}
+
+.product{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    max-height: 400px;
+    min-width: 100px;
+    background-color: black;
+    color: white;
+    margin: 10px;
+    z-index: 1;
+    padding: 20px;
+    width: 100%;
+    transition: transform 100ms ease-in-out;
+}
+.product:hover{
+    transform: scale(1.09);
+    background-color: white;
+    color: black;
+}
+.product > img{
+    width: 100%;
+    max-height: 200px;
+    object-fit: contain;
+    margin-bottom: 15px;
+}
+.product__price{
+    margin-top: 5px;
+}
+
+.product__info{
+    height: 100px;
+    margin-bottom: 15px;
+}
+.product > button{
+    background-color: #f0c14b;
+    border: 1px solid;
+    border-color:#a88734 #9c7e31 #846a29;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,23 @@
     <HeaderTwo/>
     <div class="home">
         <img class="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
-
+        <div class="home__row">
+          <Product/>
+        </div>
     </div>
+
   </div>
 </template>
 
 <script>
 import Header from '../layouts/partials/Navbar.vue';
-import HeaderTwo from '../layouts/partials/NavbarTwo.vue'
+import HeaderTwo from '../layouts/partials/NavbarTwo.vue';
+import Product  from '../components/Product.vue';
 export default {
   components:{
     Header,
-    HeaderTwo
+    HeaderTwo,
+    Product
   }
 }
 </script>
@@ -32,5 +37,14 @@ export default {
   width:100%;
   margin-bottom: -150px;
   z-index: -1;
+}
+
+.home__row{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 1;
+  max-width: 1500px;
+  margin: 0 5px;
 }
 </style>
